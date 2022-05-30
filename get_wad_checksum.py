076781e27d5bcb57834968f32d4adce8e837c075
4shockblast <shockblast4@yahoo.com>
2022-05-29 23:18:54 -0700

    Update

--- get_wad_checksum.py
+++ get_wad_checksum.py
@@ -6,4 +6,4 @@ from doomworld_downloader.utils import checksum
 
 # For manual checksums (IWADs, etc.), just use this. Could take an argument, but too lazy for now,
 # and this is easier for me to use. :P
-print(checksum('d:\\Games\\dsda-doom-dsda\\commercial\\nervenew.wad'))
+print(checksum('C:\\Games\\dsda-doom-dsda\\midtwid2.wad'))
