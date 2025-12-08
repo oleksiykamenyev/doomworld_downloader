f07ba683aeea0176a14593256f5feec192f10001
DOGEFISH\shock <shockblast4@yahoo.com>
2025-12-08 13:27:51 -0600

    Update

--- doomworld_downloader/utils.py
+++ doomworld_downloader/utils.py
@@ -22,9 +22,10 @@ from bs4 import BeautifulSoup
 from zipfile import ZipFile
 
 # CDM: Doomsday demo format
+# CLD: Zandronum co-op demo format
 # LMP: Standard demo file format (vanilla, Boom, MBF, (G)ZDoom, etc.)
 # ZDD: ZDaemon new-style demo format
-DEMO_FILE_TYPES = ['cdm', 'lmp', 'zdd']
+DEMO_FILE_TYPES = ['cdm', 'cld', 'lmp', 'zdd']
 
 HTTP_RE = re.compile(r'^https?://.+')
 HEADER_FILENAME_RE = re.compile(r'filename="(.+)"')
